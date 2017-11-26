6f10472fdf51d0c6efdbb4f2feaad6bf99e7b284
andrey_abramov <andrew.abramov00@gmail.com>
2017-11-26 14:21:03 +0300

    add eslint loader

--- source/components/my-component/index.js
+++ source/components/my-component/index.js
@@ -5,9 +5,9 @@ const b = require('b_').with('my-component');
 
 class MyComponent extends Component {
   render(props, state) {
-     return (<div className={b()}>
-       <h1 className={b('title')}>Here comes my component</h1>
-     </div>);
+    return (<div className={b()}>
+      <h1 className={b('title')}>Here comes my component</h1>
+    </div>);
   }
 }
 
